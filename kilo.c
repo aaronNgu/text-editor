9d21f91ff87f3d4009689c6f8d0f4bcd93b361b8
Aaron <Aaron@As-MacBook-Pro.local>
2021-03-05 20:11:34 -0800

    read from file multiple lines

--- kilo.c
+++ kilo.c
@@ -42,7 +42,7 @@ struct editorConfig {
     int screenrows;
     int screencols;
     int numrows;
-    erow row;
+    erow* row;
     struct termios orig_termios;
 };
 
@@ -192,6 +192,20 @@ int getWindowsSize(int *rows, int *cols)
     }
 }
 
+/*** row operations ***/
+void editorAppendRow(char *s, size_t len) 
+{
+    // stores number of erows
+    E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
+
+    int at = E.numrows;
+    E.row[at].size = len;
+    E.row[at].chars = malloc(len + 1);
+    memcpy(E.row[at].chars, s, len);
+    E.row[at].chars[len] = '\0';
+    E.numrows++;
+}
+
 /*** file i/o ***/
 
 void editorOpen(char *filename) 
@@ -202,18 +216,15 @@ void editorOpen(char *filename)
     char *line = NULL;
     size_t linecap = 0;
     ssize_t linelen;
-    linelen = getline(&line, &linecap, fp);
-    if (linelen != -1) 
+    // reads an entire line from fp, store address of buffer to line of size linecap 
+    // returns num of char read
+    while ((linelen = getline(&line, &linecap, fp)) != -1) 
     {
         // equivalent strip newline and carriage return 
         while (linelen > 0 && 
             (line[linelen - 1] == '\n' || line[linelen - 1] == '\r'))
             linelen--;
-        E.row.size = linelen;
-        E.row.chars = malloc(linelen + 1);
-        memcpy(E.row.chars, line, linelen);
-        E.row.chars[linelen] = '\0';
-        E.numrows = 1;
+        editorAppendRow(line, linelen);
     }
     free(line);
     fclose(fp);
@@ -270,9 +281,9 @@ void editorDrawRows(struct abuf *ab)
                 abAppend(ab, "~", 1);
             }
         } else {
-            int len = E.row.size;
+            int len = E.row[y].size;
             if (len > E.screencols) len = E.screencols;
-            abAppend(ab, E.row.chars, len);
+            abAppend(ab, E.row[y].chars, len);
         }
         abAppend(ab, "\x1b[K", 3);
         if (y < E.screenrows - 1) 
@@ -381,6 +392,7 @@ void initEditor()
     E.cx = 0;
     E.cy = 0;
     E.numrows = 0;
+    E.row = NULL;
     if (getWindowsSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
